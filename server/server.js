a151e9163dca21c0eabc091629e0e86fc6b65bcf
Hari Om <omhari1472@gmail.com>
2024-06-22 19:07:00 +0530

    start

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 
 const app = express()
 app.use(cors()); 
-app.use(express.json());
+app.use(express.json());  
 
 app.use((err, req, res, next) => {
   console.error(err.stack)
